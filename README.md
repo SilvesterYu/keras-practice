48604a534f07997d959d8bb8b0e0b06bbc445e02
Lifan Yu (Silvey) <ly1164@nyu.edu>
2022-06-18 22:54:50 +0400

    +

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # keras-practice
 A keras practice
 
-dataset: [download here](https://www.microsoft.com/en-us/download/details.aspx?id=54765) and extract it. Inside the unziipped folder there is a PetImages folder. Move it  inro the repository folder.
+[Original tutorial](https://keras.io/examples/vision/image_classification_from_scratch/#load-the-data-the-cats-vs-dogs-dataset)
+
+**dataset**: [download here](https://www.microsoft.com/en-us/download/details.aspx?id=54765) and unzip it. Inside the unzipped folder there is a `PetImages` folder. Move it into the repository folder so its path is `keras-practice/PetImages`.
 
 
--- keras_example1.py
+++ keras_example1.py
@@ -1,9 +1,14 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-
+import matplotlib
+from matplotlib import pyplot as plt
 import os
+import random
 
+matplotlib.use("TkAgg")
+
+# filter out badly-encoded images that do not feature the string "JFIF" in their header.
 num_skipped = 0
 for folder_name in ("Cat", "Dog"):
     folder_path = os.path.join("PetImages", folder_name)
@@ -21,3 +26,157 @@ for folder_name in ("Cat", "Dog"):
             os.remove(fpath)
 
 print("Deleted %d images" % num_skipped)
+
+# Generate a dataset
+image_size = (180, 180)
+batch_size = 32
+# Do a train-val split
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    "PetImages",
+    validation_split=0.2,
+    subset="training",
+    seed=1337,
+    image_size=image_size,
+    batch_size=batch_size,
+)
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    "PetImages",
+    validation_split=0.2,
+    subset="validation",
+    seed=1337,
+    image_size=image_size,
+    batch_size=batch_size,
+)
+
+# Visualize first 9 images
+"""
+plt.figure(figsize=(10, 10))
+for images, labels in train_ds.take(1):
+    for i in range(9):
+        ax = plt.subplot(3, 3, i + 1)
+        plt.imshow(images[i].numpy().astype("uint8"))
+        plt.title(int(labels[i]))
+        print(int(labels[i]))
+        plt.axis("off")
+        plt.show()
+        """
+
+# artificially introduce sample diversity by applying random yet realistic transformations to the training images, such as random horizontal flipping or small random rotations. This helps expose the model to different aspects of the training data while slowing down overfitting.
+data_augmentation = keras.Sequential(
+    [
+        layers.RandomFlip("horizontal"),
+        layers.RandomRotation(0.1),
+    ]
+)
+"""
+plt.figure(figsize=(10, 10))
+for images, _ in train_ds.take(1):
+    for i in range(9):
+        augmented_images = data_augmentation(images)
+        ax = plt.subplot(3, 3, i + 1)
+        plt.imshow(augmented_images[3].numpy().astype("uint8"))
+        plt.axis("off")
+        plt.show()
+"""
+
+def make_model(input_shape, num_classes):
+    inputs = keras.Input(shape=input_shape)
+    # Image augmentation block
+    x = data_augmentation(inputs)
+
+    # Entry block
+    # Here, we will standardize values to be in the [0, 1] by using a Rescaling layer at the start
+    x = layers.Rescaling(1.0 / 255)(x)
+    x = layers.Conv2D(32, 3, strides=2, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    x = layers.Conv2D(64, 3, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    previous_block_activation = x  # Set aside residual
+
+    for size in [128, 256, 512, 728]:
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+        # Project residual
+        residual = layers.Conv2D(size, 1, strides=2, padding="same")(
+            previous_block_activation
+        )
+        x = layers.add([x, residual])  # Add back residual
+        previous_block_activation = x  # Set aside next residual
+
+    x = layers.SeparableConv2D(1024, 3, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    x = layers.GlobalAveragePooling2D()(x)
+    if num_classes == 2:
+        activation = "sigmoid"
+        units = 1
+    else:
+        activation = "softmax"
+        units = num_classes
+
+    x = layers.Dropout(0.5)(x)
+    outputs = layers.Dense(units, activation=activation)(x)
+    return keras.Model(inputs, outputs)
+
+
+model = make_model(input_shape=image_size + (3,), num_classes=2)
+keras.utils.plot_model(model, show_shapes=True)
+
+# Train the model
+"""
+epochs = 3
+
+callbacks = [
+    keras.callbacks.ModelCheckpoint("save_at_{epoch}.h5"),
+]
+model.compile(
+    optimizer=keras.optimizers.Adam(1e-3),
+    loss="binary_crossentropy",
+    metrics=["accuracy"],
+)
+model.fit(
+    train_ds, epochs=epochs, callbacks=callbacks, validation_data=val_ds,
+)
+"""
+
+# load the trained weights
+model.load_weights('save_at_3.h5')
+
+# test the model on selected images
+for i in range(20):
+    j = random.randint(0, 7000)
+    k = random.randint(0,2)
+    animals = ["Cat", "Dog"]
+    try:
+        img = keras.preprocessing.image.load_img(
+            "PetImages/" + animals[k] + "/" + str(j) + ".jpg", target_size=image_size
+        )
+        plt.imshow(img)
+        plt.show()
+        img_array = keras.preprocessing.image.img_to_array(img)
+        img_array = tf.expand_dims(img_array, 0)  # Create batch axis
+
+        predictions = model.predict(img_array)
+        score = predictions[0]
+        print(
+            "This image is %.2f percent cat and %.2f percent dog."
+            % (100 * (1 - score), 100 * score)
+        )
+    except:
+        print("PetImages/Cat/" + str(j) + ".jpg does not exist, continuing...")
+        continue
+
+
