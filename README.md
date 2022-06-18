652a4eeb018fcd321a9d3264ae4dd86f7a186115
Lifan Yu (Silvey) <ly1164@nyu.edu>
2022-06-18 15:09:40 +0400

    +

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # keras-practice
 A keras practice
 
-dataset: [download here](https://www.microsoft.com/en-us/download/details.aspx?id=54765) and extract it in the repository folder
+dataset: [download here](https://www.microsoft.com/en-us/download/details.aspx?id=54765) and extract it. Inside the unziipped folder there is a PetImages folder. Move it  inro the repository folder.
 
 
--- keras_example1.py
+++ keras_example1.py
@@ -1,3 +1,23 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+
+import os
+
+num_skipped = 0
+for folder_name in ("Cat", "Dog"):
+    folder_path = os.path.join("PetImages", folder_name)
+    for fname in os.listdir(folder_path):
+        fpath = os.path.join(folder_path, fname)
+        try:
+            fobj = open(fpath, "rb")
+            is_jfif = tf.compat.as_bytes("JFIF") in fobj.peek(10)
+        finally:
+            fobj.close()
+
+        if not is_jfif:
+            num_skipped += 1
+            # Delete corrupted image
+            os.remove(fpath)
+
+print("Deleted %d images" % num_skipped)
